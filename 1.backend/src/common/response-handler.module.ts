7ca852dcb9278bd922b94e00226f2c2e54e2aa1c
HuuLoc <huuloc@192.168.x.x.non-exists.ptr.local>
2024-12-28 21:27:46 +0700

    rename

--- 1.backend/src/common/response-handler.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module, Global } from '@nestjs/common';
-import { ResponseHandlerService } from './response-handler.service';
-
-@Global() 
-@Module({
-  providers: [ResponseHandlerService],
-  exports: [ResponseHandlerService], 
-})
-export class ResponseHandlerModule {}
--- 1.backend/src/user/interfaces/users-login.interface.ts.ts
+++
@@ -1,6 +0,0 @@
-export interface UserPayload {
-  sub: number;
-  email: string;
-}
-
-
